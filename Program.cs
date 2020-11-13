f7483e83a820b666cdca5755ad1a53fcec5f23d0
Cristiano <contato@cristianoprogramador.com>
2020-11-13 08:23:39 -0300

    Cria base do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceBus_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
