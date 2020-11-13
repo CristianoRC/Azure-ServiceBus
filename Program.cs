58523c456c8e358e64e185f581efac519eff87fe
Cristiano <contato@cristianoprogramador.com>
2020-11-13 08:45:19 -0300

    Cria metodo para configurar o handler

--- Program.cs
+++ Program.cs
@@ -1,13 +1,51 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
 
 namespace ServiceBus_Example
 {
-    class Program
+    public class Program
     {
-        static async Task Main(string[] args)
+        const string QueueName = "email";
+        static IQueueClient queueClient;
+
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Teste");
+            var connectionString = Environment.GetEnvironmentVariable("SERVICEBUS");
+            Console.WriteLine(connectionString);
+            queueClient =  new QueueClient(connectionString, QueueName);
+            ConfigureHandler();
+            await queueClient.CloseAsync();
+        }
+
+        static void ConfigureHandler()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            queueClient.RegisterMessageHandler(ProcessMessage, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessage(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"ID:{message.SystemProperties.SequenceNumber}{Environment.NewLine}Corpo:{Encoding.UTF8.GetString(message.Body)}");
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
         }
     }
 }
